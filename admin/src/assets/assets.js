d24bde9053f1e833e108401ceb48d4dd563c82d9
Pratip Mitra <mitrapratip1@gmail.com>
2026-06-02 15:11:47 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -12,6 +12,8 @@ import appointments_icon from './appointments_icon.svg'
 import earning_icon from './earning_icon.svg'
 import patients_icon from './patients_icon.svg'
 
+export const backendUrl = 'https://riddhi-assurance-backend.onrender.com'
+
 export const assets = {
     add_icon,
     admin_logo,
